1dafc1e124e46ec0b0beec5bf2b51a744aa30363
Shiaobin Huang <Shiaobin@gmail.com>
2023-01-28 02:29:11 +0800

    feature: Add cors package to resolve cors problem

--- 3/backend/backend.js
+++ 3/backend/backend.js
@@ -1,7 +1,9 @@
 const fs = require('fs').promises
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.get('/course', async function (req, res) {
     const courseList = await fs.readFile('./data/course_list.json')
     res.json(JSON.parse(courseList))
